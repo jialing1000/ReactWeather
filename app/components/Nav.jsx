5284d56b4c20a0957342538ef94bb7c796b80924
jialing1000 <jialign1000@yahoo.com>
2016-11-11 21:05:08 -0800

    049 fixing localhost and 040 refactoring stateless functional component

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,17 +1,16 @@
 var React = require('react');
 var {Link, IndexLink} = require('react-router');
 
-var Nav = React.createClass({
-    render: function () {
-        return (
-            <div>
-                <h2>Nav Componetn</h2>
-                <IndexLink to="/" activeClassName="acitve" activeStyle={{fontWegith:'bold'}}>Get Weather</IndexLink> |
-                <Link to="/about" activeClassName="acitve" activeStyle={{fontWegith:'bold'}}>About</Link> |
-                <Link to="/examples" activeClassName="acitve" activeStyle={{fontWegith:'bold'}}>Examples</Link>
-            </div>
-        );
-    }
-});
+
+var Nav = () => {
+    return (
+        <div>
+            <h2>Nav Component</h2>
+            <IndexLink to="/" activeClassName="acitve" activeStyle={{fontWegith:'bold'}}>Get Weather</IndexLink> |
+            <Link to="/about" activeClassName="acitve" activeStyle={{fontWegith:'bold'}}>About</Link> |
+            <Link to="/examples" activeClassName="acitve" activeStyle={{fontWegith:'bold'}}>Examples</Link>
+        </div>
+    );
+}
 
 module.exports = Nav;
