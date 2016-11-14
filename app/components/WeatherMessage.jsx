b88bdb25fa5c44413c490b2cda4b95fbe60dbfa0
jialing1000 <jialing1000@yahoo.com>
2016-11-14 11:51:26 -0800

    055 Styling Weather Page

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -1,19 +1,9 @@
 var React = require('react');
 
-var WeatherMessage= React.createClass({
-    render: function(){
-        var {temp, location} = this.props;
-
-    }
-});
-
-var WeatherMessage = (props) => {
-	var {temp, location} = props;
+var WeatherMessage = ({temp, location}) => {
     return (
-        <div>
-            <h3>It is {temp} in {location}</h3>
-        </div>
-    );
-}
+        <h3 className="text-center">It's {temp} in {location}</h3>
+    )
+};
 
 module.exports = WeatherMessage;
