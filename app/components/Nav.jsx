299dea7666cb2f22563c7b55b1f12604bc1c3f21
jialing1000 <jialing1000@yahoo.com>
2016-11-14 10:48:17 -0800

    052 Styling Navigation

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,16 +1,45 @@
 var React = require('react');
 var {Link, IndexLink} = require('react-router');
 
-
-var Nav = () => {
-    return (
-        <div>
-            <h2>Nav Component</h2>
-            <IndexLink to="/" activeClassName="acitve" activeStyle={{fontWegith:'bold'}}>Get Weather</IndexLink> |
-            <Link to="/about" activeClassName="acitve" activeStyle={{fontWegith:'bold'}}>About</Link> |
-            <Link to="/examples" activeClassName="acitve" activeStyle={{fontWegith:'bold'}}>Examples</Link>
-        </div>
-    );
-}
+var Nav = React.createClass({
+    onSearch: function(e){
+        e.preventDefault();
+        console.log('not yet wired up!');
+    },
+    render: function(){
+        return(
+            <div className="top-bar">
+                <div className="top-bar-left">
+                    <ul className="menu">
+                        <li className="menu-text">
+                            React Weather App
+                        </li>
+                        <li>
+                            <IndexLink to="/" activeClassName="acitve" activeStyle={{fontWegith:'bold'}}>Get Weather</IndexLink>
+                        </li>
+                        <li>
+                            <Link to="/about" activeClassName="acitve" activeStyle={{fontWegith:'bold'}}>About</Link>
+                        </li>
+                        <li>
+                            <Link to="/examples" activeClassName="acitve" activeStyle={{fontWegith:'bold'}}>Examples</Link>
+                        </li>
+                    </ul>
+                </div>
+                <div className="top-bar-right">
+                    <form onSubmit={this.onSearch}>
+                        <ul className="menu">
+                            <li>
+                                <input type="search" placeholder="Search weather" />
+                            </li>
+                            <li>
+                                <input type="button" className="button" value="Get Weather" />
+                            </li>
+                        </ul>
+                    </form>
+                </div>
+            </div>
+        )
+    }
+});
 
 module.exports = Nav;
