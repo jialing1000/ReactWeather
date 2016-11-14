edd1080a760d48fdb7d9e4a30aaeec1efea81cfe
jialing1000 <jialing1000@yahoo.com>
2016-11-14 11:21:02 -0800

    053 Styling Examples Page

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,12 +1,21 @@
 var React = require('react');
+var {Link} = require('react-router');
 
-var Examples = (props) => {	
+var Examples = (props) => {
     return (
     	<div>
-        	<h3>Examples Component...</h3>
-			<p>Welcome to examples page!</p>    	
+        	<h1 className="text-center">Examples</h1>
+			<p>Here are a few example location to try out:</p>
+            <ol>
+                <li>
+                    <Link to='/?location=Philadelphia'>Philadelphia, PA</Link>
+                </li>
+                <li>
+                    <Link to='/?location=Rio'>Rio, Brazil</Link>
+                </li>
+            </ol>
     	</div>
-    );	
+    );
 }
 
 module.exports = Examples;
